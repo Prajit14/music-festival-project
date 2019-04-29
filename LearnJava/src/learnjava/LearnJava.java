8ee7a14547b959406ecfcf3729048e36acc71ca5
Prajit14 <47377528+Prajit14@users.noreply.github.com>
2019-04-29 23:14:10 +0545

    Add files via upload

---
+++ LearnJava/src/learnjava/LearnJava.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learnjava;
+
+import java.awt.FlowLayout;
+import javax.swing.*;
+
+
+/**
+ *
+ * @author PrajitGyawali
+ */
+public class LearnJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    // Create a panel 
+JPanel windowContent= new JPanel(); 
+// Set a layout manager for this panel 
+FlowLayout fl = new FlowLayout();
+windowContent.setLayout(fl); 
+// Create controls in memory  s
+    JLabel Label1 = new JLabel ("Number 1 ");
+    JTextField field1 = new JTextField(10); 
+    JLabel Label2 = new JLabel ("Number 2"); 
+    JTextField field2 = new JTextField(10); 
+    JLabel Label3 = new JLabel ("Sum");
+    JTextField result = new JTextField(10);
+    JButton go = new JButton("Add");
+// Add controls to the panel 
+windowContent.add(Label1);
+windowContent.add(Label1);
+windowContent.add(field1);
+windowContent.add(Label3);
+windowContent.add(field2);
+windowContent.add(Label3);
+windowContent.add(result);
+//windowContent.add(go);
+// Create the frame and add the panel to it 
+//JFrame frame = new JFrame(“My First Calculator”);
+    JFrame frame = new JFrame("_");
+    
+// Add the panel to the top-level container 
+//frame.setContentPane(windowContent);
+// set the size and make the window visible 
+frame.setSize(400,100); 
+frame.setVisible(true); 
+    } 
+}
